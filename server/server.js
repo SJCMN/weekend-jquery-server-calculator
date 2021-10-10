d20a45dc7b8d86f408818e06715299b714a7f21a
SJCMN <sam.clavette@gmail.com>
2021-10-10 14:00:00 -0500

    HTML layout complete

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const PORT = 5000;
 
 let app = express();
 
-const PORT = 5000;
 
+// express
 app.listen( PORT , () =>{
     console.log(`server listening on ${PORT}`);
-    
 })
+
+// .use() middleware 
+app.use(express.static('./server/public'))
+app.use(bodyParser.urlencoded({ extended: true }));
+
