e8a382b00ce62da5f9d1ff9e647e747c734ce8f6
SJCMN <sam.clavette@gmail.com>
2021-10-10 15:33:24 -0500

    each button has unique data attribute string and associated POST route

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,5 +7,78 @@ $( document ).ready(onReady);
 // event listeners
 function onReady () {
     console.log('JQ is Ready');
+    $('#addBtn').on( 'click' , addBtn )
+    $('#subtractBtn').on( 'click' , subtractBtn ) 
+    $('#multiplyBtn').on( 'click' , multiplyBtn )
+    $('#divideBtn').on( 'click' , divideBtn )
+    $('#equalBtn').on( 'click' , equalBtn )
+    $('#clearBtn').on( 'click' , clearBtn )
+}
 
+
+// POST Routes
+function addBtn(){
+    $.ajax({
+        method: 'POST',
+        url: '/math',
+        data: {
+            method: $('#addBtn').data(),
+        }
+    })
+    console.log($('#addBtn').data());
+}
+
+function subtractBtn(){
+    $.ajax({
+        method: 'POST',
+        url: '/math',
+        data: {
+            method: $('#subtractBtn').data(),
+        }
+    })
+    console.log($('#subtractBtn').data());
+}
+
+function multiplyBtn(){
+    $.ajax({
+        method: 'POST',
+        url: '/math',
+        data: {
+            method: $('#multiplyBtn').data(),
+        }
+    })
+    console.log($('#multiplyBtn').data());
+}
+
+function divideBtn(){
+    $.ajax({
+        method: 'POST',
+        url: '/math',
+        data: {
+            method: $('#divideBtn').data(),
+        }
+    })
+    console.log($('#divideBtn').data());
+}
+
+function equalBtn(){
+    $.ajax({
+        method: 'POST',
+        url: '/math',
+        data: {
+            method: $('#equalBtn').data(),
+        }
+    })
+    console.log($('#equalBtn').data());
+}
+
+function clearBtn(){
+    $.ajax({
+        method: 'POST',
+        url: '/math',
+        data: {
+            method: $('#clearBtn').data(),
+        }
+    })
+    console.log($('#clearBtn').data());
 }
